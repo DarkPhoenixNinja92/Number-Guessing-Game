5f7bb9b9d7ddb971d1b8666bd4fe87b472dc9bea
Shaun Pour <scpour@gmail.com>
2021-02-23 10:10:08 -0500

    V2

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function randomNum() {
+
+    var rand = Math.floor(Math.random() * 10 + 1);
+    var guess = prompt("Pick a number!");
+
+    while (guess != rand) {
+        alert("Sorry. That's not it! Try again.")
+        guess = prompt("Pick a number!");
+    }
+    alert("Congrats! You got it right.")
+}
+
+randomNum();
+
+var answer = prompt("Would you like to play again?");
+if (answer == "yes") {
+    randomNum();
+}
